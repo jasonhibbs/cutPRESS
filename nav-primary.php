0842a57f911e9afa1022a3acadbd11d8360b7500
Jason Hibbs <jason@jasonhibbs.co.uk>
2015-01-31 20:52:05 +0000

    cutLESS 2.2.2

--- nav-primary.php
+++ nav-primary.php
@@ -1,5 +1,5 @@
-<nav id="nav_primary" role="navigation">
-  <?php wp_nav_menu(array('theme_location' => 'nav')); ?>
+<nav role="navigation">
+  <?php wp_nav_menu(array('theme_location' => 'nav', 'menu_id' => 'nav_primary')); ?>
   <h1 class="menu_control" aria-controls="nav_primary">
     <span class="menu_text">Menu</span>
   </h1>
